0b3b386ab595e00294012ed8c38380e20176bedd
shiranjeeth <shiranjeeth619@gmail.com>
2024-12-03 17:00:58 +0530

    feat : Developed Api to get all the connections AKA friendRequest of the users

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -3,7 +3,7 @@ const express = require("express");
 const userRouter = express.Router();
 const { userAuth }   = require("../middlewares/auth.js");
 const connectionRequestModel = require('../models/connectionRequest.js');
-
+const USER_SAFE_DATA = "firstName lastName age";
 
 userRouter.get("/user/requests/received" , userAuth, async (req,res)=>{
 
@@ -23,6 +23,38 @@ userRouter.get("/user/requests/received" , userAuth, async (req,res)=>{
     }
    
 })
+
+userRouter.get("/user/connections",userAuth,async (req,res)=>{
+    try{
+        const loggedInUser = req.user;
+        const checkConnectionsList = await connectionRequestModel.find({
+            $or:[
+                {toUserId :loggedInUser._id ,status : "accepted" },
+                {fromUserId :loggedInUser._id , status : "accepted"}
+            ]
+        }).populate("fromUserId",USER_SAFE_DATA)
+        .populate("toUserId", USER_SAFE_DATA); 
+
+
+        //data should be the users except the logged in user
+        const filteredConnections = checkConnectionsList.map(connection => {
+            if (connection.fromUserId._id.toString() === loggedInUser._id.toString()) {
+                return connection.toUserId;
+            }else{
+                return connection.fromUserId;
+            }
+        });
+
+        res.status(200).json({
+            data : filteredConnections
+        })
+    }catch(err){
+        res.status(400).send("Error : "+err);
+    }
+})
+
+
+
 
 
 module.exports = userRouter;
